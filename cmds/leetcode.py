f1672422948aa4911c81c1616b8d78a5dac3f4cc
WuIFan <denny91002@gmail.com>
2020-09-01 13:20:07 +0800

    Feature: rating variation

--- cmds/leetcode.py
+++ cmds/leetcode.py
@@ -3,22 +3,34 @@ from discord.ext import commands
 from core.bot import Cog_Bot
 
 import requests
-from bs4 import BeautifulSoup
 import re
+from bs4 import BeautifulSoup
+from ast import literal_eval
 
 class LeetCode(Cog_Bot):
     emoji = [':ballot_box_with_check:',':arrow_upper_right:',':chart_with_upwards_trend:',':bar_chart:',':white_check_mark:',':writing_hand:']
+    rating_emoji = [':thumbsdown:',':punch:',':thumbsup:']
     prefer = ['Finished Contests','Rating','Global Ranking','Solved Question','Accepted Submission','Acceptance Rate']
     @commands.command()
-    async def leet(self, ctx):
+    async def lcping(self, ctx):
         await ctx.channel.send(f'{round(self.bot.latency*1000)}(ms)')
+    def checkRating(self, soup: BeautifulSoup):
+        try:
+            element = soup.find('div',class_='puiblic-profile-base')
+            rating = literal_eval(element['ng-init'].replace('pc.init',''))
+            print(rating[11][-1][0])
+            return int(rating[11][-1][0]) - int(rating[11][-2][0])
+        except Exception as e:
+            print(e)
+            return 0
     @commands.command()
-    async def get(self, ctx, account):
+    async def lcget(self, ctx, account):
         web = 'https://leetcode.com/' + account
         r = requests.get(web)
         print(r.status_code)
         await ctx.channel.send(f'status: {r.status_code}')
         soup = BeautifulSoup(r.text, 'lxml')
+        rating_variation = self.checkRating(soup)
         progesses = soup.find_all('li',class_="list-group-item")
         data = []
         for pro in progesses:
@@ -34,7 +46,11 @@ class LeetCode(Cog_Bot):
         for da in data:
             if da[0] in self.prefer:
                 print(da)
-                output += f'{self.emoji[self.prefer.index(da[0])]} {da[0]} : {da[1]}\n'
+                if da[0] == 'Rating':
+                    r_emoji = 2 if rating_variation > 0 else 0 if rating_variation < 0 else 1
+                    output += f'{self.emoji[self.prefer.index(da[0])]} {da[0]} : {da[1]}({rating_variation}{self.rating_emoji[r_emoji]})\n'
+                else:
+                    output += f'{self.emoji[self.prefer.index(da[0])]} {da[0]} : {da[1]}\n'
         await ctx.channel.send(f'{output}')
 
 # print(data)
